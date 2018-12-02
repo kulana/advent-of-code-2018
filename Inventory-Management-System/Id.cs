87ce6157abbed4833dba5e52a5d11604cc4a9e9b
JW Janse <kulana@outlook.com>
2018-12-02 11:06:53 +0100

    Added puzzle 1 solution for Dec 2

---
+++ Inventory-Management-System/Id.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventory_Management_System
+{
+    public class Id
+    {
+        public string Value { get; }
+
+        public Id(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException(nameof(input));
+            }
+            Value = input;
+        }
+
+        public bool HasOfAnyLetter(int count)
+        {
+            IDictionary<char, int> frequencyTable = new Dictionary<char, int>();
+            foreach (var c in Value)
+            {
+                Add(frequencyTable, c);
+            }
+            // get list of all characters with the specified frequency count
+            var items = frequencyTable.Values.Where(c => c == count).ToList();
+            // there should be only 1 item with this frequency
+            return items.Count > 0;
+        }
+
+        private void Add(IDictionary<char, int> frequencyTable, char c)
+        {
+            if (frequencyTable.ContainsKey(c))
+            {
+                frequencyTable[c] = frequencyTable[c] + 1;
+            }
+            else
+            {
+                frequencyTable[c] = 1;
+            }
+        }
+    }
+}
---
+++ Inventory-Management-System/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Common;
+
+namespace Inventory_Management_System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = GetInput();
+            Problem1(input);
+            //Problem2();
+        }
+
+        private static void Problem1(IList<Id> items)
+        {
+            // first part of puzzle
+            var appears2TimesCount = items.Count(id => id.HasOfAnyLetter(2));
+            var appears3TimesCount = items.Count(id => id.HasOfAnyLetter(3));
+            Console.WriteLine($"Checksum for {appears2TimesCount} * {appears3TimesCount} = {appears2TimesCount* appears3TimesCount}");
+        }
+
+        private static IList<Id> GetInput()
+        {
+            var items = new List<Id>();
+            var fileProcessor = new FileProcessor();
+            var filePath = Directory.GetCurrentDirectory() + "/input.txt";
+            fileProcessor.ReadFilePerLine(filePath, (line) =>
+            {
+                items.Add(new Id(line));
+            });
+            return items;
+        }
+    }
+}
